d6a9e67e234a60ffedda38924ee782f25558d1d1
Dopa <guengant.noe@gmail.com>
2026-06-13 15:08:32 +0200

    feat: chips informations

---
+++ data/chips.py
@@ -0,0 +1,16 @@
+TECH = "TECH"
+SUPER_TECH = "SUPER_TECH"
+
+chips = {
+    "Hedge BURG.L Chip": {"Tier": SUPER_TECH},
+    "Pond BURG.L Chip": {"Tier": SUPER_TECH},
+    "Haze BURG.L Chip": {"Tier": SUPER_TECH},
+    "Black Anthill BURG.L Chip": {"Tier": SUPER_TECH},
+    "Red Anthill BURG.L Chip": {"Tier": TECH},
+    "Sunken Outpost BURG.L Chip": {"Tier": TECH},
+    "Sandbox BURG.L Chip": {"Tier": TECH},
+    "Picnic BURG.L Chip": {"Tier": TECH},
+    "Woodpile BURG.L Chip": {"Tier": TECH},
+    "Stump BURG.L Chip": {"Tier": TECH},
+    "Forgotten BURG.L Chip": {"Tier": TECH},
+}
