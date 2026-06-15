2e0047fa1a2df3b6e82fc16e7e4ff3625c590053
Dopa <guengant.noe@gmail.com>
2026-06-15 20:38:29 +0200

    fix: all caps for TYPE

--- data/chips.py
+++ data/chips.py
@@ -2,15 +2,15 @@ TECH = "TECH"
 SUPER_TECH = "SUPER_TECH"
 
 chips = {
-    "Hedge BURG.L Chip": {"Tier": SUPER_TECH},
-    "Pond BURG.L Chip": {"Tier": SUPER_TECH},
-    "Haze BURG.L Chip": {"Tier": SUPER_TECH},
-    "Black Anthill BURG.L Chip": {"Tier": SUPER_TECH},
-    "Red Anthill BURG.L Chip": {"Tier": TECH},
-    "Sunken Outpost BURG.L Chip": {"Tier": TECH},
-    "Sandbox BURG.L Chip": {"Tier": TECH},
-    "Picnic BURG.L Chip": {"Tier": TECH},
-    "Woodpile BURG.L Chip": {"Tier": TECH},
-    "Stump BURG.L Chip": {"Tier": TECH},
-    "Forgotten BURG.L Chip": {"Tier": TECH},
+    "Hedge BURG.L Chip": {"TIER": SUPER_TECH},
+    "Pond BURG.L Chip": {"TIER": SUPER_TECH},
+    "Haze BURG.L Chip": {"TIER": SUPER_TECH},
+    "Black Anthill BURG.L Chip": {"TIER": SUPER_TECH},
+    "Red Anthill BURG.L Chip": {"TIER": TECH},
+    "Sunken Outpost BURG.L Chip": {"TIER": TECH},
+    "Sandbox BURG.L Chip": {"TIER": TECH},
+    "Picnic BURG.L Chip": {"TIER": TECH},
+    "Woodpile BURG.L Chip": {"TIER": TECH},
+    "Stump BURG.L Chip": {"TIER": TECH},
+    "Forgotten BURG.L Chip": {"TIER": TECH},
 }
